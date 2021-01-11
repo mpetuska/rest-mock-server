916310c432525ce7f5a4f0560b81120a1aa8fecf
mpetuska <martynas.petuska@outlook.com>
2021-01-11 19:11:06 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ import env from './env';
 env.then(async () => {
     const app = express();
 
-    app.listen(process.env.PORT)
+    app.listen(process.env.PORT, () => console.info(`Application running at http://localhost:${process.env.PORT}`))
 }).catch((e) => console.error('Application failed to start', e))
